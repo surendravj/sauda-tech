9ee285facd2ca8a5500ce04416db0a506148a87e
surendra <vadaparthysurendra@gmail.com>
2020-05-31 15:49:49 +0530

    Updates

---
+++ precache-manifest.7991a61579d8de5b9468b60396201b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410349e3402fe6d159d37643446d3219",
+    "url": "/sauda-tech/index.html"
+  },
+  {
+    "revision": "dbd17eddaac881e2e54f",
+    "url": "/sauda-tech/static/css/main.03583fd3.chunk.css"
+  },
+  {
+    "revision": "781f22a57373b43eb211",
+    "url": "/sauda-tech/static/js/2.f42f9d5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sauda-tech/static/js/2.f42f9d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd17eddaac881e2e54f",
+    "url": "/sauda-tech/static/js/main.f67e6e03.chunk.js"
+  },
+  {
+    "revision": "8583cedfae03272888a7",
+    "url": "/sauda-tech/static/js/runtime-main.e4b090c2.js"
+  }
+]);
